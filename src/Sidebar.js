41f58f8bb84848aecdffdda6aa3170e1048abacd
shaheer21-sys <khohneoorshaheerali21@gmail.com>
2022-11-01 21:06:10 +0530

    first commit

---
+++ src/Sidebar.js
@@ -0,0 +1,48 @@
+import React from 'react'
+import './Sidebar.css'
+import SidebarChat from './SidebarChat';
+import {Avatar, IconButton} from "@mui/material"
+import DonutLargeIcon from '@mui/icons-material/DonutLarge';
+import ChatIcon from '@mui/icons-material/Chat';
+import MoreVertIcon from '@mui/icons-material/MoreVert';
+import SearchIcon from '@mui/icons-material/Search';
+
+
+function Sidebar() {
+  return (
+    <div className='sidebar'>
+        <div className='sidebar_header'>
+            <Avatar />
+            <div className='sidebar_headerRight'>
+              <IconButton>
+               <DonutLargeIcon/>
+              </IconButton>
+              <IconButton>
+               <ChatIcon/>
+              </IconButton>
+              <IconButton>
+               <MoreVertIcon/>
+              </IconButton>
+              
+              
+            </div>
+        </div>
+        <div className='sidebar_search'>
+          <div className='sidebar_searchContainer'>
+            <SearchIcon />
+            <input placeholder="Search or Start new chat" type="text"/>
+          </div>
+        </div>
+        <div className='sidebar_chats'>
+          <SidebarChat />
+          <h1>Sidebar Chat</h1>
+          <h1>Sidebar Chat</h1>
+          <h1>Sidebar Chat</h1>
+          <h1>Sidebar Chat</h1>
+          <h1>Sidebar Chat</h1>
+        </div>
+    </div>
+  );
+}
+
+export default Sidebar
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyBN2X_ZpjORF36fwVF4jOqYd0X5nyYLB8A",
+    authDomain: "whatsappclone-ff6c0.firebaseapp.com",
+    projectId: "whatsappclone-ff6c0",
+    storageBucket: "whatsappclone-ff6c0.appspot.com",
+    messagingSenderId: "989211467176",
+    appId: "1:989211467176:web:d389eeabdd460f8702119d",
+    measurementId: "G-V70ZYSHXYN"
+  };
